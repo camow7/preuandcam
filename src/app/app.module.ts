3daa96538f056ee338d875486a66f285ffc36bef
Cameron Owen <cameron.owen@live.com.au>
2017-11-13 09:17:00 +1100

    Setup project and updated read me.

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+
+//Firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { environment } from '../environments/environment';
+export const firebaseConfig = environment.firebaseConfig;
+//Firebase end
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyASgBKXnLBSn7Bx8OaxdQUcAbiljAJrM3s",
+    authDomain: "prueandcam.firebaseapp.com",
+    databaseURL: "https://prueandcam.firebaseio.com",
+    projectId: "prueandcam",
+    storageBucket: "prueandcam.appspot.com",
+    messagingSenderId: "972402318980"
+  },
+  developmentEnv: "Production"
+};
